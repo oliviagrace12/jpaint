17dc5ab9264faeb13ee5d35544067aa24ec57148
Olivia Chisman <olivia.chisman@gmail.com>
2019-07-04 19:10:25 -0500

    refactor

--- src/view/gui/PaintMouseAdapter.java
+++ src/view/gui/PaintMouseAdapter.java
@@ -22,32 +22,40 @@ public class PaintMouseAdapter extends MouseAdapter {
     }
 
     @Override
-    public void mousePressed(MouseEvent e) {
+    public void mousePressed(MouseEvent event) {
         Rectangle rectangle = new Rectangle();
-        rectangle.setX(e.getX());
-        rectangle.setY(e.getY());
+        rectangle.setX(event.getX());
+        rectangle.setY(event.getY());
         rectangles.add(rectangle);
     }
 
     @Override
-    public void mouseReleased(MouseEvent e) {
+    public void mouseReleased(MouseEvent event) {
         if (!rectangles.empty() && rectangles.peek() != null) {
             Rectangle rectangle = rectangles.pop();
-            rectangle.setWidth(Math.abs(e.getX() - rectangle.getX()));
-            if (e.getX() < rectangle.getX()) {
-                rectangle.setX(e.getX());
-            }
-            rectangle.setHeight(Math.abs(e.getY() - rectangle.getY()));
-            if (e.getY() < rectangle.getY()) {
-                rectangle.setY(e.getY());
-            }
-            Graphics2D graphics2d = paintCanvas.getGraphics2D();
-            graphics2d.setStroke(new BasicStroke(5));
-            graphics2d.setColor(Color.BLUE);
-            graphics2d.drawRect(rectangle.getX(), rectangle.getY(), rectangle.getWidth(), rectangle.getHeight());
-            graphics2d.setColor(Color.GREEN);
-            graphics2d.fillRect(rectangle.getX(), rectangle.getY(), rectangle.getWidth(), rectangle.getHeight());
+            rectangle.setWidth(Math.abs(event.getX() - rectangle.getX()));
+            rectangle.setHeight(Math.abs(event.getY() - rectangle.getY()));
+            adjustXY(event, rectangle);
 
+            render(rectangle);
         }
+    }
+
+    private void adjustXY(MouseEvent event, Rectangle rectangle) {
+        if (event.getX() < rectangle.getX()) {
+            rectangle.setX(event.getX());
+        }
+        if (event.getY() < rectangle.getY()) {
+            rectangle.setY(event.getY());
+        }
+    }
+
+    private void render(Rectangle rectangle) {
+        Graphics2D graphics2d = paintCanvas.getGraphics2D();
+        graphics2d.setStroke(new BasicStroke(5));
+        graphics2d.setColor(Color.BLUE);
+        graphics2d.drawRect(rectangle.getX(), rectangle.getY(), rectangle.getWidth(), rectangle.getHeight());
+        graphics2d.setColor(Color.GREEN);
+        graphics2d.fillRect(rectangle.getX(), rectangle.getY(), rectangle.getWidth(), rectangle.getHeight());
     }
 }
